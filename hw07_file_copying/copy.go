abde54760445a27f6d81fd756a6beff0912582bb
Evgeniy Karasev <karasevevg@gmail.com>
2023-09-23 12:40:09 +0300

    hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +13,65 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const bufferSize = 1024
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	inFile, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return errors.New("file does not exist")
+		}
+		return ErrUnsupportedFile
+	}
+
+	fileInfo, err := inFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if fileSize == 0 {
+		return ErrUnsupportedFile
+	}
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	outFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 {
+		limit = fileSize
+	}
+	if (fileSize - offset) < limit {
+		limit = fileSize - offset
+	}
+
+	bar := pb.StartNew(int(limit))
+	buf := make([]byte, bufferSize)
+
+	for limit > 0 {
+		readBytes, err := inFile.ReadAt(buf, offset)
+		if err != nil && !errors.Is(err, io.EOF) {
+			return err
+		}
+
+		writeBytes := readBytes
+		if int64(writeBytes) > limit {
+			writeBytes = int(limit)
+		}
+		_, err = outFile.Write(buf[:writeBytes])
+		if err != nil {
+			return err
+		}
+
+		limit -= int64(readBytes)
+		offset += int64(bufferSize)
+		bar.Add(writeBytes)
+	}
+
+	bar.Finish()
+	return outFile.Chmod(fileInfo.Mode().Perm())
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -18,5 +21,29 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+
+	if err := validateInput(from, to, limit, offset); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	if err := Copy(from, to, offset, limit); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func validateInput(from, to string, limit, offset int64) error {
+	if from == "" {
+		return errors.New("from flag is required")
+	}
+	if to == "" {
+		return errors.New("to flag is required")
+	}
+	if limit < 0 {
+		return errors.New("limit flag must be greater than or equal to zero")
+	}
+	if offset < 0 {
+		return errors.New("offset flag must be greater than or equal to zero")
+	}
+	return nil
 }
