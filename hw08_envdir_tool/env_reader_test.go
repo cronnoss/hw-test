65a111b3bb0e945dbb1fc3b44ff1e5da5d854ba4
cronnoss <example@example.com>
2023-10-06 15:17:43 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,53 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	dir := "./testdata/env"
+	expected := Environment{
+		"BAR":   EnvValue{"bar", false},
+		"EMPTY": EnvValue{"", false},
+		"FOO":   EnvValue{"   foo\nwith new line", false},
+		"HELLO": EnvValue{"\"hello\"", false},
+		"UNSET": EnvValue{"", true},
+	}
+
+	t.Run("ReadDir", func(t *testing.T) {
+		envs, err := ReadDir(dir)
+
+		require.Nil(t, err)
+		require.Equal(t, expected, envs)
+	})
+
+	t.Run("DirNotExist", func(t *testing.T) {
+		_, err := ReadDir("testdata/env1")
+
+		require.NotNil(t, err)
+	})
+
+	t.Run("File name contains '='", func(t *testing.T) {
+		fileName, err := os.CreateTemp(dir, "FOO=")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer func(name string) {
+			err := os.Remove(name)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}(fileName.Name())
+
+		envs, err := ReadDir(dir)
+
+		require.Nil(t, err)
+		require.Equal(t, expected, envs)
+	})
 }
