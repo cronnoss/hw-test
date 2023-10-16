b09740451eb6f324e5ab5df9e954e88f992a4058
cronnoss <example@example.com>
2023-10-16 19:30:00 +0300

    HW09 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,14 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +17,181 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var errStr string
+	for _, e := range v {
+		errStr += fmt.Sprintf("Field: %s Error: %v\n", e.Field, e.Err)
+	}
+	return errStr
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	val := reflect.ValueOf(v)
+	typ := reflect.TypeOf(v)
+	if typ.Kind() != reflect.Struct {
+		return errors.New("expected a struct")
+	}
+	var valErrs ValidationErrors
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		fieldValue := val.Field(i)
+		tag := field.Tag.Get("validate")
+		if tag != "" && fieldValue.CanInterface() {
+			err := validateField(tag, fieldValue.Interface())
+			if err != nil {
+				valErrs = append(valErrs, ValidationError{Field: field.Name, Err: err})
+			}
+		}
+	}
+	if len(valErrs) > 0 {
+		return valErrs
+	}
 	return nil
+}
+
+func extractRule(rule string) (string, string, error) {
+	split := strings.Split(rule, ":")
+	if len(split) != 2 {
+		return "", "", fmt.Errorf("invalid rule format: %s", rule)
+	}
+	return split[0], split[1], nil
+}
+
+func validateLen(value interface{}, ruleValue string) error {
+	expectedLen, err := strconv.Atoi(ruleValue)
+	if err != nil {
+		return err
+	}
+	if reflect.TypeOf(value).Kind() == reflect.Slice {
+		return validateLenSlice(value, expectedLen)
+	}
+	return validateLenString(value, expectedLen)
+}
+
+func validateLenSlice(value interface{}, expectedLen int) error {
+	s := reflect.ValueOf(value)
+	for i := 0; i < s.Len(); i++ {
+		if len(s.Index(i).String()) != expectedLen {
+			return fmt.Errorf("expected length of %d, got %d", expectedLen, len(s.Index(i).String()))
+		}
+	}
+	return nil
+}
+
+func validateLenString(value interface{}, expectedLen int) error {
+	if len(value.(string)) != expectedLen {
+		return fmt.Errorf("expected length of %d, got %d", expectedLen, len(value.(string)))
+	}
+	return nil
+}
+
+func validateRegexp(value interface{}, ruleValue string) error {
+	s := reflect.ValueOf(value).String()
+	matched, err := regexp.Compile(ruleValue)
+	if err != nil {
+		return err
+	}
+	if !matched.MatchString(s) {
+		return fmt.Errorf("string does not match regexp %s", ruleValue)
+	}
+	return nil
+}
+
+func validateMin(value interface{}, ruleValue string) error {
+	minimum, err := strconv.Atoi(ruleValue)
+	if err != nil {
+		return err
+	}
+	if value.(int) < minimum {
+		return fmt.Errorf("number is less than minimum of %d", minimum)
+	}
+	return nil
+}
+
+func validateMax(value interface{}, ruleValue string) error {
+	maximum, err := strconv.Atoi(ruleValue)
+	if err != nil {
+		return err
+	}
+	if value.(int) > maximum {
+		return fmt.Errorf("number is more than maximum of %d", maximum)
+	}
+	return nil
+}
+
+func validateIn(value interface{}, ruleValue string) error {
+	split := strings.Split(ruleValue, ",")
+	switch reflect.TypeOf(value).Kind() { //nolint:exhaustive
+	case reflect.String:
+		return validateInString(value, split)
+	case reflect.Int:
+		return validateInInt(value, split)
+	default:
+		return fmt.Errorf("unknown type for 'in' rule")
+	}
+}
+
+func validateInString(value interface{}, split []string) error {
+	s := reflect.ValueOf(value).String()
+	if !stringInSlice(s, split) {
+		return fmt.Errorf("string %s is not in set %v", s, split)
+	}
+	return nil
+}
+
+func validateInInt(value interface{}, split []string) error {
+	n := reflect.ValueOf(value).Int()
+	ns := make([]int, len(split))
+	for i, v := range split {
+		ns[i], _ = strconv.Atoi(v)
+	}
+	if !intInSlice(int(n), ns) {
+		return fmt.Errorf("number %d is not in set %v", n, ns)
+	}
+	return nil
+}
+
+func validateField(tag string, value interface{}) error {
+	rules := strings.Split(tag, "|")
+	for _, rule := range rules {
+		ruleName, ruleValue, err := extractRule(rule)
+		if err != nil {
+			return err
+		}
+		switch ruleName {
+		case "len":
+			err = validateLen(value, ruleValue)
+		case "regexp":
+			err = validateRegexp(value, ruleValue)
+		case "min":
+			err = validateMin(value, ruleValue)
+		case "max":
+			err = validateMax(value, ruleValue)
+		case "in":
+			err = validateIn(value, ruleValue)
+		default:
+			err = fmt.Errorf("unknown tag: %s", ruleName)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func stringInSlice(s string, slice []string) bool {
+	for _, v := range slice {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func intInSlice(n int, slice []int) bool {
+	for _, v := range slice {
+		if v == n {
+			return true
+		}
+	}
+	return false
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -42,10 +42,121 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Test User",
+				Age:    25,
+				Email:  "testuser@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "10987654321"},
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz012345",
+				Name:   "Test User",
+				Age:    25,
+				Email:  "testuser@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "10987654321"},
+			},
+			expectedErr: ValidationErrors{{Field: "ID", Err: fmt.Errorf("expected length of 36, got 32")}},
+		},
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Long name_Long name_Long name_Long name_Long name_Long name_Long name_Long name_Long name_",
+				Age:    25,
+				Email:  "testuser@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "10987654321"},
+			},
+			expectedErr: nil,
+		},
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Test User",
+				Age:    70,
+				Email:  "testuser@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "10987654321"},
+			},
+			expectedErr: ValidationErrors{{Field: "Age", Err: fmt.Errorf("number is more than maximum of 50")}},
+		},
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Test User",
+				Age:    25,
+				Email:  "_test@user",
+				Role:   "admin",
+				Phones: []string{"12345678901", "10987654321"},
+			},
+			expectedErr: ValidationErrors{{Field: "Email", Err: fmt.Errorf("string does not match regexp ^\\w+@\\w+\\.\\w+$")}},
+		},
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Test User",
+				Age:    25,
+				Email:  "testuser@gmail.com",
+				Role:   "some_role",
+				Phones: []string{"12345678901", "10987654321"},
+			},
+			expectedErr: ValidationErrors{{Field: "Role", Err: fmt.Errorf("string some_role is not in set [admin stuff]")}},
+		},
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Test User",
+				Age:    25,
+				Email:  "testuser@gmail.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "4"},
+			},
+			expectedErr: ValidationErrors{{Field: "Phones", Err: fmt.Errorf("expected length of 11, got 1")}},
+		},
+		{
+			in: User{
+				ID:     "abcdefghijklmnopqrstuvwxyz0123456789",
+				Name:   "Test User",
+				Age:    25,
+				Email:  "testuser@gmail.com",
+				Role:   "admin",
+				Phones: []string{"+12345678901"},
+			},
+			expectedErr: ValidationErrors{{Field: "Phones", Err: fmt.Errorf("expected length of 11, got 12")}},
+		},
+		{
+			in: App{
+				Version: "1.0.0",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Token{
+				Header:    []byte("header"),
+				Payload:   []byte("payload"),
+				Signature: []byte("signature"),
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 404,
+				Body: "Page not found",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 400,
+				Body: "Bad Request",
+			},
+			expectedErr: ValidationErrors{{Field: "Code", Err: fmt.Errorf("number 400 is not in set [200 404 500]")}},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,7 +164,17 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
+			err := Validate(tt.in)
+			if err != nil {
+				if tt.expectedErr == nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+				if err.Error() != tt.expectedErr.Error() {
+					t.Fatalf("expected error: %v, got: %v", tt.expectedErr, err)
+				}
+			} else if tt.expectedErr != nil {
+				t.Fatalf("expected error: %v, got: %v", tt.expectedErr, err)
+			}
 			_ = tt
 		})
 	}
