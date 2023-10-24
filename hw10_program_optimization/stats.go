2370e3b7c71414fe14146513592904f9008fccd1
cronnoss <example@example.com>
2023-10-24 14:15:17 +0300

    HW10 is completed Results of benchmark runs: - before optimizations: ``` go test -v -count=1 -timeout=30s -tags bench . === RUN   TestGetDomainStat_Time_And_Memory     stats_optimization_test.go:46: time used: 306.961167ms / 300ms     stats_optimization_test.go:47: memory used: 294Mb / 30Mb ``` - after optimizations: ``` go test -v -count=1 -timeout=30s -tags bench . === RUN   TestGetDomainStat_Time_And_Memory     stats_optimization_test.go:46: time used: 102.516459ms / 300ms     stats_optimization_test.go:47: memory used: 3Mb / 30Mb ```

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,66 +1,35 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
 )
 
+//go:generate easyjson -all stats.go
 type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
+	Email string
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
+	scanner := bufio.NewScanner(r)
+	stat := make(DomainStat)
+	var user User
+	for scanner.Scan() {
+		if err := user.UnmarshalJSON(scanner.Bytes()); err != nil {
+			return nil, fmt.Errorf("error unmarshalling user: %w", err)
 		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if strings.HasSuffix(user.Email, domain) {
+			stat[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
 		}
 	}
-	return result, nil
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading input: %w", err)
+	}
+
+	return stat, nil
 }
